e90e8d2e14fcac8ed14937ef5fbe1c7de5265118
IrinaAdeniyi <irina.adeniyi@yahoo.com>
2026-04-19 11:33:35 +0100

    Connect to heroku

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@xwq$@a2@ltpx0ooulv3&%0ml&h45uiwl*vtk+vxni%!eay=05'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hello_world',
+    'about',
     ]
 
 MIDDLEWARE = [
